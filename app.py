491392ea6615d47146d6c7f479e6453a199a0175
shukur-alom <shukuralom1234@gmail.com>
2024-08-20 23:03:08 +0600

    "app.py" Remove Background

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import gradio as gr
 import cv2
 import numpy as np
+from ultralytics import SAM
+
+model = SAM("MODEL/sam2_l.pt")
 
 ROI_coordinates = {
     'x_temp': 0,
@@ -13,17 +16,32 @@ ROI_coordinates = {
 selected_sections = []
 selected_image = None
 
+
+def remove_background(img, boxes):
+    results = model(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), bboxes=boxes)
+    combined_mask = np.zeros(img.shape[:2], dtype=np.uint8)
+    for result in results:
+        for res in result:
+            mask = np.array(res.masks.xy[0])
+            cv2.fillPoly(combined_mask, np.int32([mask]), 255)
+
+    background_mask = cv2.bitwise_not(combined_mask)
+    img_no_background = cv2.bitwise_and(img, img, mask=combined_mask)
+    background = np.full_like(img, 255)
+    background = cv2.bitwise_and(background, background, mask=background_mask)
+    final_img = cv2.add(img_no_background, background)
+    return final_img
+
+
 def get_select_coordinates(img, evt: gr.SelectData):
     global selected_sections, selected_image
     sections = []
     selected_image = img
-    # update new coordinates
     ROI_coordinates['clicks'] += 1
     ROI_coordinates['x_temp'] = ROI_coordinates['x_new']
     ROI_coordinates['y_temp'] = ROI_coordinates['x_new']
     ROI_coordinates['x_new'] = evt.index[0]
     ROI_coordinates['y_new'] = evt.index[1]
-    # compare start end coordinates
     x_start = ROI_coordinates['x_new'] if (
         ROI_coordinates['x_new'] < ROI_coordinates['x_temp']) else ROI_coordinates['x_temp']
     y_start = ROI_coordinates['y_new'] if (
@@ -33,8 +51,7 @@ def get_select_coordinates(img, evt: gr.SelectData):
     y_end = ROI_coordinates['y_new'] if (
         ROI_coordinates['y_new'] > ROI_coordinates['y_temp']) else ROI_coordinates['y_temp']
     if ROI_coordinates['clicks'] % 2 == 0:
-        sections.append(
-            ((x_start, y_start, x_end, y_end), "Object Selected"))
+        sections.append(((x_start, y_start, x_end, y_end), "Object Selected"))
         selected_sections = sections
         return (img, sections)
     else:
@@ -44,29 +61,36 @@ def get_select_coordinates(img, evt: gr.SelectData):
                         "Object not Selected"))
         return (img, sections)
 
+
 def button_action():
     global selected_sections, selected_image
     if selected_sections and selected_image is not None:
         print("Button clicked! Performing action with selected sections.")
-        # Process the image (e.g., draw a rectangle around the selected ROI)
         x_start, y_start, x_end, y_end = selected_sections[0][0]
-        processed_img = cv2.rectangle(np.array(selected_image), (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
-        return processed_img, "Action performed with selected sections."
-    else:
-        return selected_image, "No sections selected."
+
+        processed_img_ = remove_background(
+            selected_image, [x_start, y_start, x_end, y_end])
+
+        cv2.imwrite("processed_image.png", cv2.cvtColor(
+            processed_img_, cv2.COLOR_BGR2RGB))
+
+        return processed_img_, "processed_image.png"
+
 
 with gr.Blocks() as demo:
     with gr.Row():
         input_img = gr.Image(label="Click")
         img_output = gr.AnnotatedImage(label="ROI",
-                                       color_map={"ROI of Face Detection": "#9987FF",
-                                                  "Click second point for ROI": "#f44336"})
+                                       color_map={"Object Selected": "#9987FF",
+                                                  "Object not Selected": "#f44336"})
     input_img.select(get_select_coordinates, input_img, img_output)
-    
+
     action_button = gr.Button("Perform Action")
-    processed_img_output = gr.Image(label="Processed Image")
-    action_output = gr.Textbox(label="Action Output")
-    action_button.click(button_action, outputs=[processed_img_output, action_output])
+    processed_img_output = gr.Image(label="Processed Image", image_mode="RGB")
+    download_button = gr.File(label="Download Processed Image")
+
+    action_button.click(button_action, outputs=[
+                        processed_img_output, download_button])
 
 if __name__ == '__main__':
     demo.launch(inbrowser=True)
